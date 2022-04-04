4b58e1ae5ce196e3a62397e01e5c2baebd36f027
Michael Simmons <msimmons86@gmail.com>
2022-04-03 17:00:49 -0700

    commit tree aff680743420ebce8d6aab9c7a334ff9b60645ea

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, name){
+  return drivers.filter(function(driver) {
+    return driver.toLowerCase() === name.toLowerCase()
+  })
+}
+
+
+function fuzzyMatch(drivers, name){
+  return drivers.filter(function(letters) {
+      return letters.startsWith(name)
+  })
+}
+
+function matchName(drivers, name) {
+  const result = drivers.filter(driver => driver.name === name)
+  return result;
+}
